cbce9f3beb4e556cfc10f0cede124a9a9bdb0fd1
Michelle Scott <scottmm374@gmail.com>
2019-10-14 10:36:27 -0400

    Figured out Dependencies Reactstrap in wrong Json. causing Tests to not run

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import Header from './components/Header'
+import PlayerCard from './components/PlayerCard'
 import App from './App';
 
 
 afterEach(rtl.cleanup);
 
-
-test('name is found', () => {
-  const {getByText} = rtl.render(<App />);
-  getByText('Country')
+test("Render the App", () => {
+  const wrapper = rtl.render(<Header />);
+ wrapper.queryByText(/Women's world cup/i)
 });
+
+// test('name is found', () => {
+//   const {getByText} = rtl.render(<App />);
+//   getByText('Country')
+// });
